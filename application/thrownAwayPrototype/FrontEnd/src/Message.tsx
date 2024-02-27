314156eba9770514836ac88576523292fa5dc622
Hoang Anh Tran <htran31@sfsu.edu>
2024-02-27 15:35:39 -0800

    Updated

--- application/thrownAwayPrototype/FrontEnd/src/Message.tsx
+++ application/thrownAwayPrototype/FrontEnd/src/Message.tsx
@@ -6,14 +6,14 @@ function Message(){
     const fabian = 'fabian';
     return(
         <div>
-            <h3>Software Engineering class SFSU <br/> Spring 2024 <br/>Section 05 <br/>Team 03</h3>
+            <h3>Software Engineering Class SFSU <br/> Spring 2024 <br/>Section 05 <br/>Team 03</h3>
             <h1>About Page <br/> GATOR CONNECT</h1>
             <ul>
                 <li><a href="/karma-gyalpo">Karma Gyalpo</a></li>
                 <li><a href="/about/jeawan">Jeawan Jang</a></li>
                 <li><a href="/about/dustin">Dustin Meza</a></li>
                 <li><a href="/about/hoanganh">Hoang-Anh Tran</a></li>
-                <li><a href="/about/fabiweiland">Fabian</a></li>
+                <li><a href="/about/fabiweiland">Fabian Weiland</a></li>
                 <li><a href="/karma-gyalpo">Ralph Quiambao</a></li>
 
 
