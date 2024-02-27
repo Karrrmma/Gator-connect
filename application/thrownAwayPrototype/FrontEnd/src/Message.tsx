2fec42326a6c57984c46d0835dc850e7980af840
Hoang Anh Tran <htran31@sfsu.edu>
2024-02-27 15:04:23 -0800

    Changed name

--- application/thrownAwayPrototype/FrontEnd/src/Message.tsx
+++ application/thrownAwayPrototype/FrontEnd/src/Message.tsx
@@ -9,12 +9,12 @@ function Message(){
             <h3>Software Engineering class SFSU <br/> Spring 2024 <br/>Section 05 <br/>Team 03</h3>
             <h1>About Page <br/> GATOR CONNECT</h1>
             <ul>
-                <li><a href="/karma-gyalpo"> Karma Gyalpo</a></li>
-                <li><a href="/karma-gyalpo">Jaewan </a></li>
-                <li><a href="/about/dustin">Dustin</a></li>
+                <li><a href="/karma-gyalpo">Karma Gyalpo</a></li>
+                <li><a href="/karma-gyalpo">Jeawan Jang</a></li>
+                <li><a href="/about/dustin">Dustin Meza</a></li>
                 <li><a href="/about/hoanganh">Hoang-Anh Tran</a></li>
-                <li><a href="/karma-gyalpo">Fabian</a></li>
-                <li><a href="/karma-gyalpo">Ralph</a></li>
+                <li><a href="/karma-gyalpo">Fabian Weiland</a></li>
+                <li><a href="/karma-gyalpo">Ralph Quiambao</a></li>
 
             </ul>
         </div>
