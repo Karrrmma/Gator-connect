bf3e4105cf8bc2bd07a015246a9aeb612f28e869
faweil <fabianweiland@arcor.de>
2024-04-14 10:53:52 -0700

    added Chat functionality - just FE

--- application/verticalPrototype/frontend/src/App.js
+++ application/verticalPrototype/frontend/src/App.js
@@ -6,6 +6,12 @@ import Post from './components/Post'; // placeholder
 import { Explore, Profile, Chat, Login, Register, Start, NewPost } from './pages';
 import useToken from './hooks/useToken';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ChatWindow from './pages/Chat/chatWindow';
+import PubChat from './pages/Chat/pubChat';
+import Channel from './pages/Chat/channel';
+
+
+
 
 function App() {
   const {token, setToken} = useToken();
@@ -35,6 +41,9 @@ function App() {
             <Route path="/explore" element={<Explore />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/chat" element={<Chat />} />
+            <Route path="/chatWindow/:name" element={<ChatWindow />} />
+            <Route path="/pubChat" element={<PubChat />} />
+            <Route path="/channel/:channel_names" element={<Channel />} />
             <Route path="/newpost" element={<NewPost />} />
           </Routes>
         </header>
--- application/verticalPrototype/frontend/src/pages/Chat/pubChat.js
+++ application/verticalPrototype/frontend/src/pages/Chat/pubChat.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ImBooks } from "react-icons/im";
+import { FaUserFriends } from "react-icons/fa";
 
-import Gru from '../../assets/images/gru.jpg';
-import Vector from '../../assets/images/vector.jpg';
-import Placeholder from '../../assets/images/placeholder_pfp.png';
-
-
+//import Gru from '../assets/images/gru.jpg';
 
 
-const Chat = () => {
+
+const PubChat = () => {
 
 
     return (
@@ -19,16 +18,12 @@ const Chat = () => {
               <Head/>
               <div className="card-body" style={{ overflowY: 'auto', height: '650px'}}>
                 <div class="d-flex justify-content-start align-items-center mb-2">
-                    <img src={Gru} class="rounded-circle" alt="placeholder pfp" style={{ width: 50, height: 50 }}></img>
-                    <Link to='/chatWindow/Fabian Weiland' className='btn btn-link text-dark text-decoration-none' style={{fontSize: '40px'}}>Fabian Weiland</Link>
+                    <FaUserFriends style={{fontSize: '50px', color: '#c15757'}}></FaUserFriends>
+                    <Link to='/channel/Find Roomates' className='btn btn-link text-dark text-decoration-none' style={{fontSize: '40px'}}>Find Roomates</Link>
                 </div>
                 <div class="d-flex justify-content-start align-items-center mb-2">
-                    <img src={Vector} class="rounded-circle" alt="placeholder pfp" style={{ width: 50, height: 50 }}></img>
-                    <Link to='/chatWindow/Jose Ortiz' className='btn btn-link text-dark text-decoration-none' style={{fontSize: '40px'}}>Jose Ortiz</Link>
-                </div>
-                <div class="d-flex justify-content-start align-items-center mb-2">
-                    <img src={Placeholder} class="rounded-circle" alt="placeholder pfp" style={{ width: 50, height: 50 }}></img>
-                    <Link to='/chatWindow/Marco Lorenz' className='btn btn-link text-dark text-decoration-none' style={{fontSize: '40px'}}>Marco Lorenz</Link>
+                    <ImBooks style={{fontSize: '50px', color: '#c15757'}}></ImBooks>
+                    <Link to='/channel/Study Help' className='btn btn-link text-dark text-decoration-none' style={{fontSize: '40px'}}>Study Help</Link>
                 </div>
                 {/* {Array.from({ length: 10 }).map((_, index) => (
                   <p key={index} style={{ color: 'black', fontSize: '20px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -51,7 +46,7 @@ const Chat = () => {
       <div className="card-body d-flex justify-content-between align-items-center">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
         <p class="bi bi-arrow-left-circle" style={{color: '#a28b39', fontSize: '30px'}}></p>
-        <h5 style={{color: 'white', fontSize: '50px'}} className="card-title mb-0">Private Chat</h5>
+        <h5 style={{color: 'white', fontSize: '50px'}} className="card-title mb-0">Public Chat</h5>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
         <Link to='/home' className="bi bi-x-circle" style={{color: 'white', fontSize: '30px'}}></Link>
       </div>
@@ -66,8 +61,8 @@ const Chat = () => {
       <div className="card-body d-flex justify-content-between align-items-center">
         <div>sdfsd</div>
         <div className='card rounded-pill' style={{backgroundColor: '#a28b39'}}>
-            <Link to='/pubChat' className="btn btn-link text-dark text-decoration-none" style={{fontSize: '30px'}}>
-            Public Chat
+            <Link to='/chat' className="btn btn-link text-dark text-decoration-none" style={{fontSize: '30px'}}>
+            Private Chat
             </Link>
         </div>
         <div>sdfsd</div>
@@ -76,4 +71,4 @@ const Chat = () => {
     );
   }
   
-  export default Chat;
+  export default PubChat;
