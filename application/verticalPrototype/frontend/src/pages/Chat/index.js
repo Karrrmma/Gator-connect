4eeaf82a6efab02b1b48bd843321b2c8b6f7e64b
faweil <fabianweiland@arcor.de>
2024-04-23 13:44:40 -0700

    new changes in Chat from testing branch

--- application/verticalPrototype/frontend/src/pages/Chat/index.js
+++ application/verticalPrototype/frontend/src/pages/Chat/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Gru from '../../assets/images/gru.jpg';
 import Dog from '../../assets/images/art10.jpg';
 import Cat from '../../assets/images/art5.jpg';
 import Placeholder from '../../assets/images/placeholder_pfp.png';
@@ -27,15 +28,15 @@ const Chat = () => {
         <div className="row justify-content-center "  >
            <div className="">
            <Head/>
-            <div className="card h-75 border-0 rounded-pill" style={{ width: '500px'}}>
+            <div className="card h-75 border-0" style={{backgroundColor: 'black', width: '500px'}}>
               
-              <div className="card-body" style={{ backgroundColor: 'black', overflowY: 'auto', height: '550px'}}>
+              <div className="card-body" style={{ backgroundColor: 'black', overflowY: 'auto', height: '450px'}}>
                 <div class="d-flex justify-content-start align-items-center" style={{ width: '100%', border: '1px solid black', padding: '10px', textAlign: 'right' }}>
                     <img src={Cat} class="rounded-circle" alt="placeholder pfp" style={{ width: 50, height: 50 }}></img>
                     <Link to='/chatWindow/Fabian Weiland' className='btn btn-link text-decoration-none' style={{color: 'white', fontSize: '25px'}}>Fabian Weiland</Link>
                 </div>
                 <div class="d-flex justify-content-start align-items-center" style={{ width: '100%', border: '1px solid black', padding: '10px', textAlign: 'right' }}>
-                    <img src={Cat} class="rounded-circle" alt="placeholder pfp" style={{ width: 50, height: 50 }}></img>
+                    <img src={Dog} class="rounded-circle" alt="placeholder pfp" style={{ width: 50, height: 50 }}></img>
                     <Link to='/chatWindow/Jose Ortiz' className='btn btn-link text-decoration-none' style={{color: 'white', fontSize: '25px'}}>Jose Ortiz</Link>
                 </div>
                 <div class="d-flex justify-content-start align-items-center" style={{ width: '100%', border: '1px solid black', padding: '10px', textAlign: 'right' }}>
@@ -43,10 +44,8 @@ const Chat = () => {
                     <Link to='/chatWindow/Marco Lorenz' className='btn btn-link text-decoration-none' style={{color: 'white', fontSize: '25px'}}>Marco Lorenz</Link>
                 </div>
               </div>
-               <Bottom/>
-               <div className='card border-0' style={{background: 'black', fontSize: '15px' }}>
-                <p>Want to chat to everyone? <br></br> Go to PUBLIC CHAT to connect more!</p>
-               </div>
+              <Bottom/>
+              <p style={{background: 'black', fontSize: '15px' }}>Want to chat to everyone? <br></br> Go to PUBLIC CHAT to connect more!</p>
             </div>
            </div>
         </div>
@@ -65,16 +64,10 @@ const Chat = () => {
 
   function Bottom(){
     return(
-    <div className="card border-0" style={{ background: 'black'}}>
-      <div className="card-body d-flex" style={{ background: 'black', justifyContent: 'center'}}>
-        
-        <div className='card rounded-pill' style={{backgroundColor: '#AD45FF'}}>
-            <Link to='/pubChat' className="btn btn-link text-decoration-none" style={{color: 'white', fontSize: '20px'}}>
-            Public Chat
-            </Link>
-        </div>
-        
-      </div>
+    <div className="card-body d-flex" style={{ background: 'black', justifyContent: 'center'}}>
+          <Link to='/pubChat' className="btn btn-link rounded-pill text-decoration-none" style={{backgroundColor: '#AD45FF', color: 'white', fontSize: '20px'}}>
+          Public Chat
+          </Link>      
     </div>
     );
   }
