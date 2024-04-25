1759dedb06e597e648b944baac22e6db91b67728
laerfugi <rquiambao@mail.sfsu.edu>
2024-04-25 02:13:51 -0700

    new post confirmation added

--- application/verticalPrototype/frontend/src/pages/Profile/NewPostPopup.js
+++ application/verticalPrototype/frontend/src/pages/Profile/NewPostPopup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function NewPostPopup({ onClose }) {
     const [post, setPost] = useState('');
+    const [confirmation, setConfirmation] = useState('');
     
     const handleExit = () => {
         onClose();
@@ -15,26 +16,35 @@ function NewPostPopup({ onClose }) {
         e.preventDefault();
         console.log(post);
         setPost('');
-        onClose();
+        // onClose();
+        setConfirmation('A new post has been created!');
     };
     
     return (
         <div className="friend-list-popup">
             <div className="popup-inner">
-                <h2>ADD NEW POST</h2>
-                <p className="popup-subtitle">Share anything to connect more!</p>
-                <form onSubmit={handleSubmit}>
-                    <textarea className="post-input" value={post} onChange={handlePost} placeholder="Enter your content here" />
-                    <div className="image-upload-container">
-                        <textarea className="post-upload" placeholder="Add an image (optional)" />
-                        <button className="upload-button" type="button">Upload</button>
-                    </div>
-                    <div className="button-group">
-                        <button className="cancel-button" type="button" onClick={handleExit}>CANCEL</button>
-                        <button className="post-button" type="submit">POST</button>
-                    </div>
-                </form>
-                <button className="close-button" onClick={handleExit}>X</button>
+                {confirmation ? (
+                    <>
+                    <p className='mt-5 mb-5'>{confirmation}</p>
+                    <button className="close-button" onClick={handleExit}>X</button>
+                    </>
+                ) : (
+                    <>
+                        <h2>ADD NEW POST</h2>
+                        <p className="popup-subtitle">Share anything to connect more!</p>
+                        <form onSubmit={handleSubmit}>
+                            <textarea className="post-input" value={post} onChange={handlePost} placeholder="Enter your content here" />
+                            <div className="image-upload-container">
+                                <textarea className="post-upload" placeholder="Add an image (optional)" />
+                                <button className="upload-button" type="button">Upload</button>
+                            </div>
+                            <div className="button-group">
+                                <button className="cancel-button" type="button" onClick={handleExit}>CANCEL</button>
+                                <button className="post-button" type="submit">POST</button>
+                            </div>
+                        </form>
+                    </>
+                )}
             </div>
         </div>
     );
