a90ab25297d538f70b6461c704c06c9f424c6768
laerfugi <rquiambao@mail.sfsu.edu>
2024-05-14 00:10:08 -0700

    added extra check to prevent backend from crashing

--- application/betaPrototype/backend/routes/verifyToken.js
+++ application/betaPrototype/backend/routes/verifyToken.js
@@ -3,6 +3,11 @@ const  jwt = require('jsonwebtoken');
 require('dotenv/config');
 
 exports.verifyToken = async (req, res, next) =>{
+    if (!req.headers.authorization) {
+        console.log('No authorization header provided');
+        return next(createError(401, "You are not authenticated!"));
+      }
+      
     const token = req.headers.authorization.split(' ')[1];
     const mysecretkey = process.env.JWT;
     if (!token){
